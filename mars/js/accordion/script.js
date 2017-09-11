fc32c9eeecadeeb7377edd10029c295d37ce05d8
zfw203916 <15328628@qq.com>
2017-09-11 18:08:42 +0800

    add html

---
+++ mars/js/accordion/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+	/* This code is executed after the DOM has been completely loaded */
+
+	/* Changing thedefault easing effect - will affect the slideUp/slideDown methods: */
+	$.easing.def = "easeOutSine";
+
+	/* Binding a click event handler to the links: */
+	//$('li.button a ').click(function(e){
+	$("a[id^='dropdown-li']").click(function(e){
+		/* Finding the drop down list that corresponds to the current section: */
+		var dropDown = $(this).parent().next();
+		
+		/* Closing all other drop down sections, except the current one */
+		$('.dropdown').not(dropDown).slideUp('slow');
+		
+		dropDown.slideToggle('slow');
+		
+		/* Preventing the default event (which would be to navigate the browser to the link's address) */
+		e.preventDefault();
+	});
+	
+});
